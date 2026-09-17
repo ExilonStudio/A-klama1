1be0593f4bb9203babf3a7f8b2b7700268718d01
ExilonStudio <tvbizim44@gmail.com>
2026-09-17 19:02:39 +0300

    Create Config.cs

---
+++ Config.cs
@@ -0,0 +1,12 @@
+namespace ExilonTracker
+{
+    public static class Config
+    {
+        public static string StudioName => "Exilon Studio";
+        public static string EngineVersion => "3.0.1-UTF98258";
+        public static string StudioStatus => "Tam Bağımsız (Active)";
+        public static string GithubReadmeUrl => "https://github.com/ExilonStudio/A-klama1/blob/main/README.md";
+        public static string GoogleFormUrl => "https://forms.gle/1axVBVDmN4ReDQrE9";
+        public static string OfficialWeb => "https://esws.oneapp.dev/";
+    }
+}
