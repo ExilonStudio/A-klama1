7855b146b7671498323e235b3f740135cf5b5c94
ExilonStudio <tvbizim44@gmail.com>
2026-09-17 19:02:31 +0300

    Add EngineMonitor class with engine state methods

---
+++ EngineMonitor.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ExilonTracker
+{
+    public class EngineMonitor
+    {
+        public string GetEngineState()
+        {
+            return "Exilon Engine 3.0 Core Active (Direct3D 12)";
+        }
+
+        public bool CheckGithubRelease()
+        {
+            return true;
+        }
+    }
+}
