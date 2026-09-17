5bc0b3951fdf0e49e519a77db0ff05d4d747abfc
ExilonStudio <tvbizim44@gmail.com>
2026-09-17 19:02:49 +0300

    Add WebNavigator class to open URLs

---
+++ WebNavigator.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ExilonTracker
+{
+    public static class WebNavigator
+    {
+        public static void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+            }
+        }
+    }
+}
